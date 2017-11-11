2e9018e287382b104829ad67181238e06adb2a9b
StefaniAlmeida <almeidastefani099@gmail.com>
2017-11-10 22:40:15 -0200

    Update App.py

--- thecoolplace/App.py
+++ thecoolplace/App.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect ( ' usuário.db ' )
 cursor = conn.cursor ()
 
 # Criando tabela Usuario
-cursor.execute ( "" "
+cursor.execute ( """
 CREATE TABLE usuario ()
     id int NOT NULL,
     nome varchar (100) NOT NULL,
@@ -14,20 +14,20 @@ CREATE TABLE usuario ()
     cidade VARCHAR (150) NÃO NULL,
     data_nascimento DATA NÃO NULL,
     );
-"" " )
+""" )
 print ( ' tabela criada ' )
 # Finalizando criacao da tabela usuário
 
 # inserir usuário
-cursor.execute ( "" "
+cursor.execute ( """
 Insira no usuário (id, nome, email, senha, sexo, cidade, data_nascimento)
 valores (2, 'nicolas', nicolas @ gmail ',' 12345 ',' masculino ',' soledade ', 2001/08/30)
-"" " )
+""" )
 
 # selecionar usuário
-cursor.execute ( "" "
+cursor.execute ( """
 selecione * do usuário
-"" " )
+""" )
 
 para linha em cursor.fetchall ():
     imprimir (linha)
@@ -35,18 +35,18 @@ para linha em cursor.fetchall ():
 id_usuario =  1
 
 # update usuário
-cursor.execute ( "" " "
+cursor.execute ( """
 Atualizar usuário
 onde id =?
-"" " , (id_usuario,))
+""" , (id_usuario,))
 
 print ( ' dados atualizados ' )
 
 # delete user
-cursor.execute ( "" " "
+cursor.execute ( """
 eliminar do usuário
 onde id =?
-"" " , (id_usuario,))
+""" , (id_usuario,))
 
 #tabela usuario
 select * from [TB_usuario]
@@ -54,26 +54,26 @@ delet from [TB_usuario] where id = ?
 update [TB_usuario] set (id_usuario) values(?) where id = ?
 
 # Criando tabela Mensagem
-cursor.execute ( "" "
+cursor.execute ( """
 CREATE TABLE mensagem (
     id int NOT NULL,
     destinatario varchar (150) NÃO NULL,
     rementente varchar (150) NOT NULL,
     data_envio data NÃO NULL
     );
-"" " )
+""" )
 print ( ' tabela criada ' )
 
 # inserir mensagem
-cursor.execute ( "" " "
+cursor.execute ( """
 inserir na mensagem (id, destinatario, rementente, data_envio)
 valores (3'nicolas ',' stefani ', 2017/10/25)
-"" " )
+""" )
 
 # selecionar mensagem
-cursor.execute ( "" "
+cursor.execute ( """
 selecione * da mensagem
-"" " )
+""" )
 
 para linha em cursor.fetchall ():
     imprimir (linha)
@@ -81,17 +81,17 @@ para linha em cursor.fetchall ():
 id_mensagem =  6
 
 # atualização mensagem
-cursor.execute ( "" "
+cursor.execute ( """
 mensagem de atualização
 onde id =?
-"" " , (id_mensagem,))
+""" , (id_mensagem,))
 print ( ' Dados atualizados ' )
 
 # delete message
-cursor.execute ( "" "
+cursor.execute ( """
 excluir da mensagem
 onde id =?
-"" " , (id_mensagem,))
+""" , (id_mensagem,))
 
 select * from [TB_mensagem]
 delet from [TB_mensagem] where id = ?
@@ -99,24 +99,24 @@ update [TB_mensagem] set (id_mensagem) values(?) where id = ?
 
 
 # criando um conversor de tabela
-cursor.execute ( "" "
+cursor.execute ( """
 CREATE TABLE chat (
     conversa varchar (100) NOT NULL,
     id int NOT NULL
     );
-"" " )
+""" )
 print ( ' Tabela criada! ' )
 
 # inserir bate-papo
-cursor.execute ( "" "
+cursor.execute ( """
 Insira no chat (conversa, id)
 valores ('oi', 7)
-"" " )
+""" )
 
 # selecione o bate-papo
-cursor.execute ( "" "
+cursor.execute ( """
 selecione * a partir do chat
-"" " )
+""" )
 
 para linha em cursor.fetchall ():
     imprimir (linha)
@@ -124,41 +124,41 @@ para linha em cursor.fetchall ():
 id_chat =  9
 
 # atualização
-cursor.execute ( "" "
+cursor.execute ( """
 atualizar conversa
 onde id =?
-"" " , (id_chat,))
+""" , (id_chat,))
 print ( ' dados atualizados ' )
 
 # delete chat
-cursor.execute ( "" " "
+cursor.execute ( """
 excluir do bate-papo
 onde id =?
-"" " , (id_chat,))
+""" , (id_chat,))
 
 select * from [TB_chat]
 delet from [TB_chat] where id = ?
 update [TB_chat] set (id_chat) values(?) where id = ?
 
 # criando uma alimentação de tabela
-cursor.execute ( "" " "
+cursor.execute ( """
 Criar alimentação de tabela (
     publicaçao varchar (500) não nulo,
     id int null
     );
-"" " )
+""" )
 print ( ' Tabela criada ' )
 
 # inserir feed
-cursor.execute ( "" "
+cursor.execute ( """
 inserir no feed (publicacao, id)
 valores ('publicacao', 5)
-"" " )
+""" )
 
 # selecione o feed
-cursor.execute ( "" " "
+cursor.execute ( """
 selecione * do feed
-"" " )
+""" )
 
 para linha em cursor.fetchall ():
     imprimir (linha)
@@ -167,17 +167,17 @@ para linha em cursor.fetchall ():
 id_feed =  4
 
 # atualização de feed
-cursor.execute ( "" "
+cursor.execute ( """
 feed de atualização
 onde id =?
-"" " , (id_feed,))
+""" , (id_feed,))
 print ( ' dados atualizados ' )
 
 # delete feed
-cursor.execute ( "" " "
+cursor.execute ( """
 excluir do feed
 onde id =?
-"" " , (id_feed,))
+""" , (id_feed,))
 
 select * from [TB_feed]
 delet from [TB_feed] where id = ?
@@ -185,24 +185,24 @@ update [TB_feed] set (id_feed) values(?) where id = ?
 
 
 # criando tabela visibiliade
-cursor.execute ( "" " "
+cursor.execute ( """
 criar tabela de visibilidade (
     publica varchar (100) não nulo,
     id int null,
     privado varchar (100) não é nulo
     );
-"" " )
+""" )
 print ( ' tabela criada ' )
 
 # inserção visual
-cursor.execute ( "" " "
+cursor.execute ( """
 inserir na visibilidade (publica, id, privada)
 valores ('visivel', 20, 'privado')
-"" " )
+""" )
 
-cursor.execute ( "" "
+cursor.execute ( """
 selecione * da visibilidade
-"" " )
+""" )
 
 para linha em cursor.fetchall ():
     imprimir (linha)
@@ -211,17 +211,17 @@ para linha em cursor.fetchall ():
 id_visibilidade =  30
 
 # update visibiliade
-cursor.execute ( "" " "
+cursor.execute ( """
 atualizar a visibilidade
 onde id =?
-"" " , (id_visibilidade,))
+""" , (id_visibilidade,))
 print ( ' dados atualizados ' )
 
 # delete visibiliade
-cursor.execute ( "" " "
+cursor.execute ( """
 excluir da visibilidade
 onde id =?
-"" " , (id_visibilidade,))
+""" , (id_visibilidade,))
 conn.commit ()
 conn.close ()
 
