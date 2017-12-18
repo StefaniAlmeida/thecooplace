026d103c9bd4a13cd7710710b9d241d296826f81
StefaniAlmeida <almeidastefani099@gmail.com>
2017-12-18 09:01:38 -0200

    Update Postagem.py

--- thecoolplace/Model/Postagem.py
+++ thecoolplace/Model/Postagem.py
@@ -7,16 +7,4 @@ class Postagem():
         self.texto = texto
         self.id = id
 
-    def insert(self):
-        conn = mysql.connector.connect(**config)
-        cursor = conn.cursor()
-
-        cursor.execute("""
-            INSERT INTO tb_postagem(usuario_id, texto)
-            VALUES(%s, %s)
-        """, (self.usuario, self.texto))
-
-        self.id = cursor.lastrowid
-
-        conn.commit()
-        cursor.close()
+   
